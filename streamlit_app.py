83138ff52ca510640a75ff2e7249ec211662a566
Srijon Pal <53072058+srijonpal@users.noreply.github.com>
2023-08-03 12:57:41 +0530

    Update streamlit_app.py
    using variables in api

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Advice")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # normalise json to table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # print the table
 streamlit.dataframe(fruityvice_normalized)
+
+
